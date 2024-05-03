cc64c62676ec4a9b5d3235bd0770986a1394166f
“mahmudhasanador” <mahmudhasanador@gmail.com>
2024-05-03 14:12:59 +0600

    fixed the issue of closing modal after showing error message

--- assets/app.js
+++ assets/app.js
@@ -4,9 +4,8 @@ const submitBtn = document.getElementById("submit-btn");
 const saveBtn = document.getElementById("save-btn");
 const addUserBtn = document.getElementById("add-user-btn");
 const nameInput = document.getElementById("nameInput");
-// const modal = document.getElementById("exampleModal");
 const emailInput = document.getElementById("emailInput");
-
+const closeBtn = document.getElementById("close-btn");
 
 const tableBody = document
   .getElementById("myTable")
@@ -24,14 +23,14 @@ let nameInputValue,
   findIndex,
   myModal;
 
-function setItem() {
+function storeUsers() {
   users = JSON.stringify(users);
   localStorage.setItem("users", users);
 }
-function getItem() {
+function getUsers() {
   users = localStorage.getItem("users");
 }
-function parseItem(){
+function parseUsers() {
   users = JSON.parse(users);
 }
 
@@ -53,20 +52,19 @@ const addUser = (name, email) => {
   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
   const isValid = emailRegex.test(email);
-  console.log(isValid);
-  
+
   if (!isValid) {
     alert("Enter a valid email");
     return;
   }
 
   const id = uuidv4();
-  getItem();
+  getUsers();
 
   if (users === null) {
     users = [];
   } else {
-    parseItem();
+    parseUsers();
   }
   const newUser = {
     name: name,
@@ -80,53 +78,57 @@ const addUser = (name, email) => {
   };
 
   users.push(newUserWithId);
-  setItem();
+  storeUsers();
 };
 
-
+// create  modal instance
+myModal = bootstrap.Modal.getOrCreateInstance(
+  document.getElementById("exampleModal")
+  
+);
 
 submitBtn.addEventListener("click", function (e) {
   e.preventDefault();
   nameInputValue = nameInput.value.trim();
   emailInputValue = emailInput.value.trim();
   addUser(nameInputValue, emailInputValue);
-  
-  
+
   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
   const isValid = emailRegex.test(emailInputValue);
 
-  if (nameInputValue != "" && nameInputValue != undefined && emailInputValue!="" && emailInputValue!=undefined && isValid) {
-    
+  if (
+    nameInputValue != "" &&
+    nameInputValue != undefined &&
+    emailInputValue != "" &&
+    emailInputValue != undefined &&
+    isValid
+  ) {
     setTimeout(() => {
       myModal.hide();
-      document.querySelector('.modal-backdrop').remove();
+      document.querySelector(".modal-backdrop").remove();
       nameInput.value = "";
-  emailInput.value = "";
+      emailInput.value = "";
     }, 10);
   }
 
   displayUsers();
-  // nameInput.value = "";
-  // emailInput.value = "";
-  myModal.show(); 
-
 });
 
 // add user button
 addUserBtn.addEventListener("click", function () {
+  myModal.show();
   submitBtn.style.display = "block";
   saveBtn.style.display = "none";
-  myModal = bootstrap.Modal.getOrCreateInstance(document.getElementById('exampleModal')); 
 });
 
 // display user function
 const displayUsers = () => {
   tableBody.innerHTML = "";
-  getItem();
+  getUsers();
 
   if (users !== null) {
-    parseItem();
+    parseUsers();
 
     for (var i = 0; i < users.length; i++) {
       let newRow = document.createElement("tr");
@@ -142,7 +144,7 @@ const displayUsers = () => {
         newRow.appendChild(newCell);
       }
 
-      actionsCell.innerHTML = `<div id ="myActions"> <button class="btn edit-Button" data-bs-toggle="modal" data-bs-target="#exampleModal"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pen" viewBox="0 0 16 16">
+      actionsCell.innerHTML = `<div id ="myActions"> <button class="btn edit-Button"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pen" viewBox="0 0 16 16">
       <path d="m13.498.795.149-.149a1.207 1.207 0 1 1 1.707 1.708l-.149.148a1.5 1.5 0 0 1-.059 2.059L4.854 14.854a.5.5 0 0 1-.233.131l-4 1a.5.5 0 0 1-.606-.606l1-4a.5.5 0 0 1 .131-.232l9.642-9.642a.5.5 0 0 0-.642.056L6.854 4.854a.5.5 0 1 1-.708-.708L9.44.854A1.5 1.5 0 0 1 11.5.796a1.5 1.5 0 0 1 1.998-.001m-.644.766a.5.5 0 0 0-.707 0L1.95 11.756l-.764 3.057 3.057-.764L14.44 3.854a.5.5 0 0 0 0-.708z"/>
     </svg></button>
       <button class="btn delete-Button"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
@@ -159,17 +161,18 @@ const displayUsers = () => {
       deleteButton.addEventListener("click", deleteUser);
 
       editButton.addEventListener("click", function (e) {
-        // myModal = bootstrap.Modal.getOrCreateInstance(document.getElementById('exampleModal')); 
+        myModal.show();
+
         submitBtn.style.display = "none";
         saveBtn.style.display = "block";
         const selectedUser = e.target.closest("tr");
 
         userId = selectedUser.getAttribute("data-user-id");
 
-        getItem();
+        getUsers();
 
         if (users !== null) {
-          parseItem();
+          parseUsers();
           users.forEach((user) => {
             if (userId === user.id) {
               editNameInp = document.getElementById("nameInput");
@@ -180,10 +183,9 @@ const displayUsers = () => {
             }
           });
 
-          saveBtn.removeEventListener("click", editUser);
+          saveBtn.removeEventListener("click", modalHandler);
 
-          // Add event listener for the "Save changes" button
-          saveBtn.addEventListener("click", editUser);
+          saveBtn.addEventListener("click", modalHandler);
         }
       });
     }
@@ -208,53 +210,61 @@ function editUser() {
     return;
   }
 
-  
   findIndex = users.findIndex((user) => user.id === userId);
   users[findIndex].name = editNameInp.value;
   users[findIndex].email = editEmailInp.value;
 
-  setItem();
-
-  // if (editNameInp.value != "" && editNameInp.value != undefined && editEmailInp.value!="" && editEmailInp.value!=undefined && isValid) {
-    
-  //   setTimeout(() => {
-  //     myModal.hide();
-  //     document.querySelector('.modal-backdrop').remove();
-  
-  //   }, 10);
-  // }
-  displayUsers();
-      editNameInp.value = "";
-      editEmailInp.value = "";
-  // myModal.show();
+  storeUsers();
 }
-
 
 // delete user function
 const deleteUser = (e) => {
   const selectedUser = e.target.closest("tr");
   const userId = selectedUser.getAttribute("data-user-id");
-  getItem();
+  getUsers();
   if (users !== null) {
-    parseItem();
+    parseUsers();
 
     users = users.filter(function (user) {
       return user.id != userId;
     });
   }
 
-  setItem();
+  storeUsers();
   displayUsers();
 };
 
-
-
-// };
-
+//load user function
 const loadUsers = () => {
-  users = getItem() || [];
+  users = getUsers() || [];
   displayUsers();
 };
 
 // Call loadUsers when the page loads
 window.addEventListener("load", loadUsers);
+
+closeBtn.addEventListener("click", function () {
+  nameInput.value = "";
+  emailInput.value = "";
+});
+
+function modalHandler() {
+  editUser();
+
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  const isValid = emailRegex.test(editEmailInp.value);
+  if (
+    editNameInp.value != "" &&
+    editNameInp.value != undefined &&
+    editEmailInp.value != "" &&
+    editEmailInp.value != undefined &&
+    isValid
+  ) {
+    setTimeout(() => {
+      myModal.hide();
+      editNameInp.value = "";
+      editEmailInp.value = "";
+    }, 10);
+  }
+  displayUsers();
+}
